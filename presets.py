118951190844bc9d5e0fc3a097569c139d65fa67
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-08-02 17:24:35 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,13 @@
 class Presets(object):
     WELCOME_MESSAGE = """
 <b>Hello.. </b>{}
+<b>
+➥ MAIN CHANNEL [Click Me](https://t.me/+BSzJAUCQshw1N2Nl)
+</b> <b>
+➥ PO*N [Click Me](https://t.me/hornhubbot)
+</b> <b>
+➥ MOVIES [Click Me](https://t.me/guleriabot)
+</b>
  
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
