5cf5235d99fde63b2c2f48f65b5551299d288225
as6823917 <66206385+as6823917@users.noreply.github.com>
2022-08-02 17:20:03 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,13 +1,7 @@
 class Presets(object):
     WELCOME_MESSAGE = """
 <b>Hello.. </b>{}
- <b>
-➥ MAIN CHANNEL [Click Me](https://t.me/+BSzJAUCQshw1N2Nl)
-</b> <b>
-➥ PO*N [Click Me](https://t.me/hornhubbot)
-</b> <b>
-➥ MOVIES [Click Me](https://t.me/guleriabot)
-</b>
+ 
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
